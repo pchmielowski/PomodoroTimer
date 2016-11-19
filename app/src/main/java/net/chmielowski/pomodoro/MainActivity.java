d2e9f5e8ed1da95fb386cf1c61068ffd6d7576cb
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-11-19 13:57:08 +0100

    AlarmManager used

--- app/src/main/java/net/chmielowski/pomodoro/MainActivity.java
+++ app/src/main/java/net/chmielowski/pomodoro/MainActivity.java
@@ -1,7 +1,9 @@
 package net.chmielowski.pomodoro;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -14,24 +16,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView mTextField = (TextView) findViewById(R.id.main_tv);
 
+
         findViewById(R.id.main_button_start).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        new CountDownTimer(30000, 1000) {
-                            public void onTick(long millisUntilFinished) {
-                                mTextField.setText(String.valueOf(
-                                        millisUntilFinished / 1000));
-                            }
-
-                            public void onFinish() {
-                                mTextField.setText("Done!");
-                            }
-                        }.start();
-
-
+                        int repeatTime = 5;
+                        AlarmManager processTimer =
+                                (AlarmManager) getSystemService(
+                                        ALARM_SERVICE);
+                        Intent intent = new Intent(
+                                getApplicationContext(), Receiver.class);
+                        PendingIntent pendingIntent =
+                                PendingIntent.getBroadcast(
+                                        getApplicationContext(), 0, intent,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                );
+                        processTimer.setRepeating(
+                                AlarmManager.RTC_WAKEUP,
+                                System.currentTimeMillis(),
+                                repeatTime * 1000, pendingIntent
+                        );
                     }
                 }
         );
     }
 }
+
---
+++ app/src/main/java/net/chmielowski/pomodoro/Receiver.java
@@ -0,0 +1,14 @@
+package net.chmielowski.pomodoro;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Receiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("pchm", "received");
+    }
+}
