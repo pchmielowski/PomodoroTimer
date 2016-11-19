92562a629b148f469e1e35b2f8c58bbd4831fc8d
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-11-19 13:48:36 +0100

    Working hardcoded 30 sec timer

--- app/src/main/java/net/chmielowski/pomodoro/MainActivity.java
+++ app/src/main/java/net/chmielowski/pomodoro/MainActivity.java
@@ -1,7 +1,10 @@
 package net.chmielowski.pomodoro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView mTextField = (TextView) findViewById(R.id.main_tv);
+
+        findViewById(R.id.main_button_start).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new CountDownTimer(30000, 1000) {
+                            public void onTick(long millisUntilFinished) {
+                                mTextField.setText(String.valueOf(
+                                        millisUntilFinished / 1000));
+                            }
+
+                            public void onFinish() {
+                                mTextField.setText("Done!");
+                            }
+                        }.start();
+
+
+                    }
+                }
+        );
     }
 }
